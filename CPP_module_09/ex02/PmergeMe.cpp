440a903931ff06e0396ebb2396a81bd2c0b33fa1
hameur <hameur@student.1337.ma>
2023-03-21 14:51:30 +0000

    almost finish

--- CPP_module_09/ex02/PmergeMe.cpp
+++ CPP_module_09/ex02/PmergeMe.cpp
@@ -2,7 +2,6 @@
 
 mergeMe::mergeMe(std::string *args, size_t length){
     this->length = length;
-    std::cout << "l = " << this->length << "\n";
     if (this->parseDigits(args) < 0)
         exit(1);
 }
@@ -16,7 +15,7 @@ const mergeMe& mergeMe::operator=(const mergeMe &other){
 int mergeMe::isDigit(std::string arg){
     for(size_t i = 0; i < arg.length(); i++){
         if (arg[i] < '0' || arg[i] > '9')
-            return std::cout << arg << "   aaaaaa\n", -1;
+            return -1;
     }
     return 0;
 }
@@ -25,7 +24,6 @@ int mergeMe::parseDigits(std::string *args){
     for(size_t i = 0; i < this->length; i++){
         if (isDigit(args[i]) < 0)
             return std::cerr << "ERROR Bad Arg\n" , -1;
-        std::cout << "av = " << args[i] << "\n";
         _vec.push_back(std::atoi(args[i].c_str()));
         _list.push_back(std::atoi(args[i].c_str()));
     }
@@ -37,123 +35,132 @@ mergeMe::~mergeMe(){ ; }
 
 //************************************************//
 
-void mergeMe::mergeSortin(std::list<int> &_list){
-    if (_list.size() < 2)
-        return ;
-    size_t middle = _list.size() / 2;
-    std::list<int> left;
-    std::list<int> right;
+// void mergeMe::mergeSortin(std::list<int> &_list){
+//     if (_list.size() < 2)
+//         return ;
+//     size_t middle = _list.size() / 2;
+//     std::list<int> left;
+//     std::list<int> right;
 
-    size_t i = 0;
-    std::list<int>::iterator it = _list.begin();
-    while (i < _list.size()){
-        if (i < middle)
-            left.push_back(*it);
-        else 
-            right.push_back(*it);
-    }
-    this->mergeSortin(left);
-    this->mergeSortin(right);
-    merge(_list, left, right);
-}
+//     size_t i = 0;
+//     std::list<int>::iterator it = _list.begin();
+//     while (it !=  _list.end() &&i < _list.size()){
+//         if (i < middle)
+//             left.push_back(*it);
+//         else 
+//             right.push_back(*it);
+//         it++;
+//         i++;
+//     }
+//     this->mergeSortin(left);
+//     this->mergeSortin(right);
+//     merge(_list, left, right);
+// }
 
-void mergeMe::merge(std::list<int> &list, std::list<int>&left, std::list<int>&right){
-    size_t l = 0, r = 0;
-    std::list<int>::iterator it = list.begin(), itL = left.begin(), itR = right.begin();
-    while (l < left.size() && r < right.size()){
-        if (*itL < *itR){
-            *it = *itL;
-            it++;itL++;
-        }else 
-            *it = *itR;
-            it++;itR++;
-    }
-    while (l < left.size()){
-            *it = *itL;
-            it++;itL++;
-    }
-    while (r < right.size()){
-            *it = *itR;
-            it++;itR++;
-    }
-}
+// void mergeMe::merge(std::list<int> &list, std::list<int>&left, std::list<int>&right){
+//     size_t l = 0, r = 0;
+//     std::list<int>::iterator it = list.begin(), itL = left.begin(), itR = right.begin();
+//     while (l < left.size() && r < right.size()){
+//         if (*itL < *itR){
+//             *it = *itL;
+//             it++;itL++;l++;
+//         }else {
+//             *it = *itR;
+//             it++;itR++;r++;
+//     }}
+//     while (l < left.size()){
+//             *it = *itL;
+//             it++;itL++;l++;
+//     }
+//     while (r < right.size()){
+//             *it = *itR;
+//             it++;itR++;r++;
+//     }
+// }
 
-void mergeMe::mergeSortin(std::vector<int> &_vec){
-    if (_vec.size() < 2)
-        return ;
-    size_t middle = _vec.size() / 2;
-    std::vector<int> left;
-    std::vector<int> right;
-    size_t i = 0;
-    std::cout << "-------------------------------\n";
-    std::vector<int>::iterator it = _vec.begin();
-    while (it !=  _vec.end() && i < _vec.size()){
-        std::cout << "it = " << *it << std::endl;
-        if (i < middle)
-            left.push_back(*it);
-        else
-            right.push_back(*it);
-        it++;
-        i++;
-    }
-    std::vector<int>::iterator it2 = left.begin();
-    std::vector<int>::iterator it3 = right.begin();
+// void mergeMe::mergeSortin(std::vector<int> &_vec){
+//     if (_vec.size() < 2)
+//         return ;
+//     size_t middle = _vec.size() / 2;
+//     std::vector<int> left;
+//     std::vector<int> right;
+//     size_t i = 0;
+//     std::vector<int>::iterator it = _vec.begin();
+//     while (it !=  _vec.end() && i < _vec.size()){
+//         if (i < middle)
+//             left.push_back(*it);
+//         else
+//             right.push_back(*it);
+//         it++;
+//         i++;
+//     }
+//     this->mergeSortin(left);
+//     this->mergeSortin(right);
+//     this->merge(_vec, left, right);
+// }
 
-    while (it2 != left.end())
-        {std::cout << "left = " << *it2 << "\n"; it2++;}
-    while (it3 != right.end())
-        {std::cout << "right = " << *it3 << "\n"; it3++;}
-    this->mergeSortin(left);
-    this->mergeSortin(right);
-    this->merge(_vec, left, right);
-
-}
-
-void mergeMe::merge(std::vector<int>&vec, std::vector<int>&left, std::vector<int>&right){
-    size_t l = 0, r = 0;
-    std::vector<int>::iterator it = vec.begin(), itL = left.begin(), itR = right.begin();
-    std::cout << "lft  and right == == " << left.size() << "  " << right.size() << "\n";
-    
-    while (l < left.size() && r < right.size()){
+// void mergeMe::merge(std::vector<int>&vec, std::vector<int>&left, std::vector<int>&right){
+//     size_t l = 0, r = 0;
+//     std::vector<int>::iterator it = vec.begin(), itL = left.begin(), itR = right.begin();
+//     while (l < left.size() && r < right.size()){
         
-        if (*itL < *itR){
-            *it = *itL;
-            it++;itL++;l++;
-        }else 
-            *it = *itR;
-            it++;itR++;r++;
-    }
-    while (l < left.size()){
-            *it = *itL;
-            it++;itL++;l++;
-    }
-    while (r < right.size()){
-            *it = *itR;
-            it++;itR++;r++;
-    }
-}
+//         if (*itL < *itR){
+//             *it = *itL;
+//             it++;itL++;l++;
+//         }else{ 
+//             *it = *itR;
+//             it++;itR++;r++;
+//     }}
+//     while (l < left.size()){
+//             *it = *itL;
+//             it++;itL++;l++;
+//     }
+//     while (r < right.size()){
+//             *it = *itR;
+//             it++;itR++;r++;
+//     }
+// }
 
 
 void mergeMe::makeTime(){
-    time(&_time);
+    struct timeval	__time;
+
+	gettimeofday(&__time, NULL);
+	
+    _time = __time.tv_sec * 1000000 + __time.tv_usec;;
 }
 
 void mergeMe::printCurrentTime(){
+    struct timeval	__time;
+
+	gettimeofday(&__time, NULL);
     time_t current;
-    time(&current);
+    current = __time.tv_sec * 1000000 + __time.tv_usec;;
     std::cout << "Time :: " << current - _time << "\n";
 }
 
 
 void mergeMe::beginSortin(){
+
+    std::vector<int>::iterator it = _vec.begin();
+    std::cout << "Before:\t";
+    while (it != _vec.end()){
+        std::cout  << *it << " ";
+        it++;
+    }
+    std::cout << std::endl;;
     this->makeTime();
     this->mergeSortin(this->_vec);
-    std::cout << "hello\n";
-    std::vector<int>::iterator it = _vec.begin();
+
+
+    it = _vec.begin();
+    std::cout << "After:\t";
     while (it != _vec.end()){
-        std::cout  << "it = "<< *it << std::endl;
+        std::cout  << *it << " ";
         it++;
-    }printCurrentTime();
+    }
+    std::cout << std::endl;;
+    printCurrentTime();
 
     this->makeTime();
     this->mergeSortin(this->_list);
--- CPP_module_09/ex02/PmergeMe.hpp
+++ CPP_module_09/ex02/PmergeMe.hpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <sys/time.h>
 #include <list>
 #include <ctime>
 
@@ -22,12 +23,56 @@ class mergeMe{
         int parseDigits(std::string *args);
         int isDigit(std::string arg);
         
-        void merge(std::vector<int>&, std::vector<int>&, std::vector<int>&);
-        virtual void merge(std::list<int>&, std::list<int>&, std::list<int>&); 
+        // void merge(std::vector<int>&, std::vector<int>&, std::vector<int>&);
+        // virtual void merge(std::list<int>&, std::list<int>&, std::list<int>&); 
         void makeTime();
         void printCurrentTime();;
-        virtual void mergeSortin(std::list<int> &);
-        virtual void mergeSortin(std::vector<int>&);
+        // virtual void mergeSortin(std::list<int> &);
+        // virtual void mergeSortin(std::vector<int>&);
         void beginSortin();
+
+        template <typename Container>
+        void mergeSortin(Container &cont){
+            if (cont.size() < 2)
+                return ;
+            size_t middle = cont.size() / 2;
+            size_t i = 0;
+            Container left;
+            Container right;
+            typename Container::iterator it = cont.begin();
+            while (it !=  cont.end() && i < cont.size()){
+                if (i < middle)
+                    left.push_back(*it);
+                else
+                    right.push_back(*it);
+                it++;
+                i++;
+            }
+            this->mergeSortin(left);
+            this->mergeSortin(right);
+            this->merge(cont, left, right);
+        }
+        
+        template <typename Container>
+        void merge(Container &cont, Container &left, Container &right){
+            size_t l = 0, r = 0;
+            typename Container::iterator it = cont.begin(), itL = left.begin(), itR = right.begin();;
+            while (l < left.size() && r < right.size()){
+                if (*itL < *itR){
+                    *it = *itL;
+                    it++;itL++;l++;
+                }else{ 
+                    *it = *itR;
+                    it++;itR++;r++;
+            }}
+            while (l < left.size()){
+                    *it = *itL;
+                    it++;itL++;l++;
+            }
+            while (r < right.size()){
+                    *it = *itR;
+                    it++;itR++;r++;
+            }
+        }
 
 };
